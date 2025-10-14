caab1439d5ef9992408e5602484c6f587c8b5ea5
MuhammadRafliEriyanto <raflimhmmd621@gmail.com>
2025-10-14 10:55:29 +0700

    updateurl

--- app/routes/auth_user.py
+++ app/routes/auth_user.py
@@ -168,7 +168,7 @@ def reset_password():
 
     return jsonify({"msg": "Password updated successfully"}), 200
 
-N8N_WEBHOOK_URL = "https://n8n.gitstraining.com/webhook/chatbotcb"
+N8N_WEBHOOK_URL = "https://n8n.gitstraining.com/webhook/chatbotgenius"
 
 @auth_user_bp.route('/chatbot', methods=['POST'])
 @jwt_required()
